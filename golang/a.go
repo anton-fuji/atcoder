8a37c64320b9e21c46254fe901d6ae9be7908e0a
Fuji <fujiji.entire1414@gmail.com>
2025-03-09 14:11:00 +0900

    D prob.

--- golang/a.go
+++ golang/a.go
@@ -1,29 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
-
-var s string
+import "fmt"
 
 func main() {
-	fmt.Scan(&s)
+	var n int
+	fmt.Scan(&n)
 
-	firstA := -1
-	for i := 0; i < len(s); i++ {
-		if s[i] == 'A' {
-			firstA = i
-			break
-		}
+	A := make([]int, n)
+	count := make(map[int]int)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&A[i])
+		count[A[i]]++
 	}
 
-	lastZ := -1
-	for i := len(s) - 1; i >= 0; i-- {
-		if s[i] == 'Z' {
-			lastZ = i
-			break
-		}
+	uniqueCount := len(count)
+
+	duplicates := n - uniqueCount
+
+	if duplicates%2 == 1 {
+		uniqueCount--
 	}
 
-	fmt.Println(lastZ - firstA + 1)
+	fmt.Println(uniqueCount)
 }
