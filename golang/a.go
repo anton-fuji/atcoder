69b94b5becbef117321b7e92e77a569dbc7c7d58
Fuji <fujiji.entire1414@gmail.com>
2025-03-10 21:31:59 +0900

    clear

--- golang/a.go
+++ golang/a.go
@@ -1,25 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	var n int
-	fmt.Scan(&n)
 
-	A := make([]int, n)
-	count := make(map[int]int)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&A[i])
-		count[A[i]]++
-	}
-
-	uniqueCount := len(count)
-
-	duplicates := n - uniqueCount
-
-	if duplicates%2 == 1 {
-		uniqueCount--
-	}
-
-	fmt.Println(uniqueCount)
 }
