dcd7dbcabd09cb9e1d8826aecd2eb4bef8d7806b
Fuji <fujiji.entire1414@gmail.com>
2025-03-11 18:52:22 +0900

    algorithm用のmdファイル作成

--- golang/a.go
+++ golang/a.go
@@ -2,25 +2,16 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
-
-func bubbleSort(arr []int) {
-	n := len(arr)
-	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-1-i; j++ {
-			if arr[j] > arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
-			}
-		}
-	}
-}
 
 func main() {
 	var a, b, c int
 	fmt.Scan(&a, &b, &c)
 
-	array := []int{a, b, c}
-	bubbleSort(array)
+	nums := []int{a, b, c}
 
-	fmt.Println(array[0], array[1], array[2])
+	sort.Ints(nums)
+
+	fmt.Println(nums[0], nums[1], nums[2])
 }
