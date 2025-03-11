528e7904b5a3441e65645983f3439bb1a8654e23
Fuji <fujiji.entire1414@gmail.com>
2025-03-11 17:55:45 +0900

    add

--- golang/a.go
+++ golang/a.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var S int
+	fmt.Scan(&S)
+
+	hours := S / 3600
+	minutes := (S % 3600) / 60
+	seconds := S % 60
+
+	fmt.Printf("%d:%d:%d\n", hours, minutes, seconds)
 }
