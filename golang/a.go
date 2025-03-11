b1cc2eca63c6738cf04b3f766eeaa5cf920bf9bd
Fuji <fujiji.entire1414@gmail.com>
2025-03-11 18:42:36 +0900

    bubble sort

--- golang/a.go
+++ golang/a.go
@@ -1,17 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func bubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
 
 func main() {
-	var a, b int
-	fmt.Scan(&a, &b)
+	var a, b, c int
+	fmt.Scan(&a, &b, &c)
 
-	switch {
-	case a < b:
-		fmt.Println("a < b")
-	case a > b:
-		fmt.Println("a > b")
-	case a == b:
-		fmt.Println("a == b")
-	}
+	array := []int{a, b, c}
+	bubbleSort(array)
+
+	fmt.Println(array[0], array[1], array[2])
 }
