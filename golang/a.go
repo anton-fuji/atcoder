67ef894072c565aacdd6098727bb65c148ef7b14
Fuji <fujiji.entire1414@gmail.com>
2025-03-15 23:55:58 +0900

    AOJ ITP1_4_D

--- golang/a.go
+++ golang/a.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	minNum := math.MaxInt64
+	maxNum := math.MinInt64
+	sum := 0
+
+	for i := 0; i < n; i++ {
+		var num int
+		fmt.Scan(&num)
+
+		if num < minNum {
+			minNum = num
+		}
+		if num > maxNum {
+			maxNum = num
+		}
+		sum += num
+	}
+	fmt.Printf("%d %d %d\n", minNum, maxNum, sum)
 }
