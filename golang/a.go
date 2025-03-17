659e95a64939034ea31b0957715b555cd088a924
Fuji <fujiji.entire1414@gmail.com>
2025-03-17 19:10:02 +0900

    fix

--- golang/a.go
+++ golang/a.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
@@ -13,9 +12,15 @@ func main() {
 		if H == 0 && W == 0 {
 			break
 		}
-
 		for i := 0; i < H; i++ {
-			fmt.Println(strings.Repeat("#", W))
+			for j := 0; j < W; j++ {
+				if (i+j)%2 == 0 {
+					fmt.Print("#")
+				} else {
+					fmt.Print(".")
+				}
+			}
+			fmt.Println()
 		}
 		fmt.Println()
 	}
