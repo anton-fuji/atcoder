3d3bcfb8a27c80795df3fa36a699445b849affe8
Fuji <fujiji.entire1414@gmail.com>
2025-03-18 22:59:38 +0900

    不足しているカードを見つける

--- golang/a.go
+++ golang/a.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	ranks := []string{"S", "H", "C", "D"}
+	cardExists := make(map[string]bool)
+
+	var n int
+	fmt.Scan(&n)
+
+	for i := 0; i < n; i++ {
+		var rank string
+		var num int
+		fmt.Scan(&rank, &num)
+		cardExists[fmt.Sprintf("%s %d", rank, num)] = true
+	}
+
+	for _, rank := range ranks {
+		for num := 1; num <= 13; num++ {
+			card := fmt.Sprintf("%s %d", rank, num)
+			if !cardExists[card] {
+				fmt.Println(card)
+			}
+		}
+	}
 }
