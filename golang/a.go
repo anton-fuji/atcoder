6424772a1c69f0cad224f59c30f6651611ac7e9d
Fuji <fujiji.entire1414@gmail.com>
2025-03-18 23:43:38 +0900

    公舎の入居者数(ムズイ)

--- golang/a.go
+++ golang/a.go
@@ -1,29 +1,35 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	ranks := []string{"S", "H", "C", "D"}
-	cardExists := make(map[string]bool)
+	appartment := make([][][]int, 4)
+	for i := range appartment {
+		appartment[i] = make([][]int, 3)
+		for j := range appartment[i] {
+			appartment[i][j] = make([]int, 10)
+		}
+	}
 
 	var n int
 	fmt.Scan(&n)
 
 	for i := 0; i < n; i++ {
-		var rank string
-		var num int
-		fmt.Scan(&rank, &num)
-		cardExists[fmt.Sprintf("%s %d", rank, num)] = true
+		var b, f, r, v int
+		fmt.Scan(&b, &f, &r, &v)
+		appartment[b-1][f-1][r-1] += v
 	}
 
-	for _, rank := range ranks {
-		for num := 1; num <= 13; num++ {
-			card := fmt.Sprintf("%s %d", rank, num)
-			if !cardExists[card] {
-				fmt.Println(card)
-			}
+	for b := 0; b < 4; b++ {
+		if b > 0 {
+			fmt.Println("####################")
 		}
+		for f := 0; f < 3; f++ {
+			for r := 0; r < 10; r++ {
+				fmt.Print(" ", appartment[b][f][r])
+			}
+			fmt.Println()
+		}
+
 	}
 }
