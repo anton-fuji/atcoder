696be18288540f6b59afebd4cc9a36d0fabeb731
Fuji <fujiji.entire1414@gmail.com>
2025-03-21 16:18:26 +0900

    clear

--- golang/a.go
+++ golang/a.go
@@ -3,35 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	var n, m int
-	fmt.Scan(&n, &m)
-
-	// create matrix_A
-	A := make([][]int, n)
-	for i := 0; i < n; i++ {
-		A[i] = make([]int, m)
-		for j := 0; j < m; j++ {
-			fmt.Scan(&A[i][j])
-		}
-	}
-
-	// creat fact_B
-	B := make([]int, m)
-	for i := 0; i < m; i++ {
-		fmt.Scan(&B[i])
-	}
-
-	// c = A[][] * B[]
-	c := make([]int, n)
-	for i := 0; i < n; i++ {
-		sum := 0
-		for j := 0; j < m; j++ {
-			sum += A[i][j] * B[j]
-		}
-		c[i] = sum
-	}
-
-	for i := 0; i < n; i++ {
-		fmt.Println(c[i])
-	}
+	fmt.Println("Hello World!")
 }
