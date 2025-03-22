793e8115ee77c05cf2304dd08ab9164db3973d02
Fuji <fujiji.entire1414@gmail.com>
2025-03-22 18:44:32 +0900

    sort algo renew

--- golang/a.go
+++ golang/a.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func sorting(arr []int) {
+	n := len(arr)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	var n int
+	fmt.Scan(&n)
+
+	num := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&num[i])
+	}
+	sorting(num)
+
+	for _, v := range num {
+		fmt.Print(v, " ")
+	}
 }
