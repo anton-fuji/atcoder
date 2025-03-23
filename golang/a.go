5a51558eefef5498cc7dbc1a4e82289d54e0f141
Fuji <fujiji.entire1414@gmail.com>
2025-03-23 20:43:32 +0900

    fix

--- golang/a.go
+++ golang/a.go
@@ -1,35 +1,33 @@
 package main
 
-import (
-	"fmt"
-)
-
-func judge(a, b string) int {
-	if a == b {
-		return 0
-	}
-
-	if (a == "G" && b == "C") ||
-		(a == "C" && b == "P") ||
-		(a == "P" && b == "G") {
-		return 1
-	}
-	return 2
-}
+import "fmt"
 
 func main() {
-	var n int
-	fmt.Scan(&n)
+	for {
+		var m, f, r int
+		fmt.Scan(&m, &f, &r)
 
-	count := 0
-	for i := 0; i < n; i++ {
-		var a, b string
-		fmt.Scan(&a, &b)
+		if m == -1 && f == -1 && r == -1 {
+			break
+		}
 
-		result := judge(a, b)
-		if result == 1 {
-			count += 1
+		switch {
+		case m == -1 || f == -1:
+			fmt.Println("F")
+		case m+f >= 80:
+			fmt.Println("A")
+		case 65 <= m+f:
+			fmt.Println("B")
+		case 50 <= m+f:
+			fmt.Println("C")
+		case 30 <= m+f:
+			if r >= 50 {
+				fmt.Println("C")
+			} else {
+				fmt.Println("D")
+			}
+		default:
+			fmt.Println("F")
 		}
 	}
-	fmt.Println(count)
 }
