63362dc68589f10be619ceff85780185853d5a7a
Fuji <fujiji.entire1414@gmail.com>
2025-03-29 16:15:37 +0900

    clear

--- golang/a.go
+++ golang/a.go
@@ -1,34 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var n, m int
-	fmt.Scan(&n, &m)
+	var a, b int
+	fmt.Scan(&a, &b)
 
-	A := make([][]int, n)
-	for i := range A {
-		A[i] = make([]int, m)
-		for j := 0; j < m; j++ {
-			fmt.Scan(&A[i][j])
-		}
-	}
-	B := make([][]int, n)
-	for i := range B {
-		B[i] = make([]int, m)
-		for j := 0; j < m; j++ {
-			fmt.Scan(&B[i][j])
-		}
-	}
-	for i := 0; i < n; i++ {
-		for j := 0; j < m; j++ {
-			if j > 0 {
-				fmt.Print(" ")
-			}
-			fmt.Print(A[i][j] + B[i][j])
-		}
-		fmt.Println()
-	}
+	d := a / b
+	r := a % b
+	f := float64(a) / float64(b)
+
+	fmt.Printf("%d %d %.5f\n", d, r, f)
 }
