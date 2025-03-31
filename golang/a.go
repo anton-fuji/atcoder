0ced2da17406377b4a6af93410fe93ec8dbdc698
Fuji <fujiji.entire1414@gmail.com>
2025-03-31 16:13:51 +0900

    clear

--- golang/a.go
+++ golang/a.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"unicode"
-)
-
 func main() {
-	counts := make([]int, 26)
 
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		line := scanner.Text()
-		for _, c := range line {
-			if unicode.IsLetter(c) {
-				lower := unicode.ToLower(c)
-				index := lower - 'a'
-				counts[index]++
-			}
-		}
-	}
-
-	for i := 0; i < 26; i++ {
-		fmt.Printf("%c : %d\n", 'a'+i, counts[i])
-	}
 }
