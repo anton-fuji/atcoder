d0c93cccd10ae90dc15ad909344872ef0801695f
Fuji <fujiji.entire1414@gmail.com>
2025-04-05 21:58:05 +0900

    prob B

--- ABC400/a.go
+++ ABC400/a.go
@@ -1,16 +1,14 @@
 package main
 
-import "fmt"
+// func main() {
+// 	var a int
+// 	fmt.Scan(&a)
 
-func main() {
-	var a int
-	fmt.Scan(&a)
+// 	row := 400 / a
 
-	row := 400 / a
-
-	if 400%a == 0 {
-		fmt.Println(row)
-	} else {
-		fmt.Println(-1)
-	}
-}
+// 	if 400%a == 0 {
+// 		fmt.Println(row)
+// 	} else {
+// 		fmt.Println(-1)
+// 	}
+// }
--- ABC400/b.go
+++ ABC400/b.go
@@ -1,5 +1,30 @@
 package main
 
-// func main() {
-// 	fmt.Println()
-// }
+import (
+	"fmt"
+)
+
+func main() {
+	var n, m int
+	fmt.Scan(&n, &m)
+
+	var limit = int64(1e9)
+
+	sum := int64(1)
+	ans := int64(1)
+
+	for i := 0; i < m; i++ {
+		if ans > limit/int64(n) {
+			fmt.Println("inf")
+			return
+		}
+		ans *= int64(n)
+
+		if sum > limit-ans {
+			fmt.Println("inf")
+			return
+		}
+		sum += ans
+	}
+	fmt.Println(sum)
+}
