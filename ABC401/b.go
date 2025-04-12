cd25636ed670d8c3df1c7733cc9a02e36df6446a
Fuji <fujiji.entire1414@gmail.com>
2025-04-12 22:45:38 +0900

    abc401 B

---
+++ ABC401/b.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	loggedIn := false
+	errCnt := 0
+
+	for i := 0; i < n; i++ {
+		var s string
+		fmt.Scan(&s)
+
+		switch s {
+		case "login":
+			loggedIn = true
+		case "logout":
+			loggedIn = false
+		case "public":
+		case "private":
+			if !loggedIn {
+				errCnt++
+			}
+		}
+	}
+	fmt.Println(errCnt)
+}
