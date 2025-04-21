6b5948fda8e88669e2186b9c5bbb6c7c015e7c2d
Fuji <fujiji.entire1414@gmail.com>
2025-04-21 12:25:28 +0900

    bufio practice

--- golang/a.go
+++ golang/a.go
@@ -1,27 +1,37 @@
-// https://atcoder.jp/contests/abc083/tasks/abc083_b
+// https://onlinejudge.u-aizu.ac.jp/problems/ITP1_9_A
 package main
 
-import "fmt"
-
-func digitSum(n int) int {
-	sum := 0
-	for n > 0 {
-		sum += n % 10
-		n /= 10
-	}
-	return sum
-}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	var n, a, b int
-	fmt.Scan(&n, &a, &b)
+	var w string
+	fmt.Scan(&w)
 
-	ans := 0
-	for i := 0; i < n; i++ {
-		s := digitSum(i)
-		if a <= s && b >= s {
-			ans += i
+	w = strings.ToLower(w)
+
+	// 複数入力の場合、'bufio'を使う。1行ずつ読み込む。
+	sc := bufio.NewScanner(os.Stdin)
+	cnt := 0
+
+	for sc.Scan() {
+		line := sc.Text()
+		if line == "END_OF_TEXT" {
+			break
+		}
+
+		wds := strings.Fields(line)
+
+		for _, wd := range wds {
+			if strings.ToLower(wd) == w {
+				cnt++
+			}
 		}
 	}
-	fmt.Println(ans)
+
+	fmt.Println(cnt)
 }
