1b8f21052137404e2412c8bc7ecc0f59a7c6ac76
Fuji <fujiji.entire1414@gmail.com>
2025-04-22 21:49:08 +0900

    abc375 A fix

--- golang/a.go
+++ golang/a.go
@@ -4,17 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
+	n, _ := strconv.Atoi(sc.Text())
+
 	sc.Scan()
-	n := sc.Text()
+	s := sc.Text()
 
 	cnt := 0
-	for _, s := range n {
-		if s == '.' {
+	for i := 0; i < n-1; i++ {
+		if s[i] == '#' && s[i+1] == '.' && s[i+2] == '#' {
 			cnt++
 		}
 	}
