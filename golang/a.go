2989006c423e158214bdcdc539c70ffdd0d434cf
Fuji <fujiji.entire1414@gmail.com>
2025-04-22 20:47:37 +0900

    abc402 A

--- golang/a.go
+++ golang/a.go
@@ -1,51 +1,22 @@
-// https://atcoder.jp/contests/typical90/tasks/typical90_d
 package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
-	"strconv"
-	"strings"
+	"unicode"
 )
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
-	line := sc.Text()
-	part := strings.Split(sc.Text(), " ")
-	h, _ := strconv.Atoi(part[0])
-	w, _ := strconv.Atoi(part[1])
+	s := sc.Text()
 
-	A := make([][]int, h)
-	rowSum := make([]int, h)
-	colSum := make([]int, w)
-
-	for i := 0; i < h; i++ {
-		sc.Scan()
-		line = sc.Text()
-		part = strings.Split(line, " ")
-		A[i] = make([]int, w)
-		for j := 0; j < w; j++ {
-			val, _ := strconv.Atoi(part[j])
-			A[i][j] = val
-			rowSum[i] += val
-			colSum[j] += val
+	result := ""
+	for _, r := range s {
+		if unicode.IsUpper(r) {
+			result += string(r)
 		}
 	}
-
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
-
-	for i := 0; i < h; i++ {
-		for j := 0; j < w; j++ {
-			//行iの合計 + 列jの合計 - A[i][j]
-			sum := rowSum[i] + colSum[j] - A[i][j]
-			writer.WriteString(strconv.Itoa(sum))
-			if j != w-1 {
-				writer.WriteByte(' ')
-			}
-		}
-		writer.WriteByte('\n')
-	}
-
+	fmt.Println(result)
 }
