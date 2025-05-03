22515919f7363078979e1478b068d84256759119
Fuji <fujiji.entire1414@gmail.com>
2025-05-03 19:32:41 +0900

    401 B

--- golang/a.go
+++ golang/a.go
@@ -1,44 +1,31 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	out := bufio.NewWriter(os.Stdout)
-	defer out.Flush()
+	var n int
+	fmt.Scan(&n)
 
-	var n, m, Q int
-	fmt.Fscan(in, &n, &m, &Q)
+	loggedIn := false
+	errCnt := 0
 
-	all := make([]bool, n+1)
-	perm := make([]map[int]bool, n+1)
-	for i := 1; i <= n; i++ {
-		perm[i] = make(map[int]bool)
-	}
+	for i := 0; i < n; i++ {
+		var s string
+		fmt.Scan(&s)
 
-	for i := 0; i < Q; i++ {
-		var typ, x, y int
-		fmt.Fscan(in, &typ)
-		switch typ {
-		case 1:
-			fmt.Fscan(in, &x, &y)
-			if !all[x] {
-				perm[x][y] = true
-			}
-		case 2:
-			fmt.Fscan(in, &x)
-			all[x] = true
-		case 3:
-			fmt.Fscan(in, &x, &y)
-			if all[x] || perm[x][y] {
-				fmt.Fprintln(out, "Yes")
-			} else {
-				fmt.Fprintln(out, "No")
+		switch s {
+		case "login":
+			loggedIn = true
+		case "logout":
+			loggedIn = false
+		case "public":
+		case "private":
+			if !loggedIn {
+				errCnt++
 			}
 		}
 	}
+	fmt.Println(errCnt)
 }
