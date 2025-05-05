52ce1156798ef6822db80facf7857dd176e75c6b
Fuji <fujiji.entire1414@gmail.com>
2025-05-05 13:26:49 +0900

    404 B

--- golang/a.go
+++ golang/a.go
@@ -1,4 +1,3 @@
-// https://onlinejudge.u-aizu.ac.jp/problems/ITP1_9_C
 package main
 
 import (
@@ -7,29 +6,60 @@ import (
 	"os"
 )
 
-func main() {
-	in := bufio.NewReader(os.Stdin)
-
-	var n int
-	fmt.Fscan(in, &n)
-
-	sT := 0
-	sH := 0
-
-	// 辞書順で大きいものを比較する時は、ただの文字列比較で十分
+func rotate90(grid []string) []string {
+	n := len(grid)
+	rotated := make([]string, n)
 	for i := 0; i < n; i++ {
-		var cardT, cardH string
-		fmt.Fscan(in, &cardT, &cardH)
+		row := make([]byte, n)
+		for j := 0; j < n; j++ {
+			// 90度回転の変換式：新しい[i][j] = 元の[n-j-1][i]
+			row[j] = grid[n-j-1][i]
+		}
+		rotated[i] = string(row)
+	}
+	return rotated
+}
 
-		switch {
-		case cardT > cardH:
-			sT += 3
-		case cardT < cardH:
-			sH += 3
-		case cardT == cardH:
-			sT++
-			sH++
+// gridの差分をとる
+func difference(a, b []string) int {
+	n := len(a)
+	count := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if a[i][j] != b[i][j] {
+				count++ // 色が違えば1回の変更が必要 => 必要変更回数をカウント
+			}
 		}
 	}
-	fmt.Println(sT, sH)
+	return count
+}
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	var n int
+	fmt.Sscanf(sc.Text(), "%d", &n)
+
+	s := make([]string, n)
+	for i := 0; i < n; i++ {
+		sc.Scan()
+		s[i] = sc.Text()
+	}
+
+	t := make([]string, n)
+	for i := 0; i < n; i++ {
+		sc.Scan()
+		t[i] = sc.Text()
+	}
+
+	ops := n * n * 10 //十分大きい初期値
+	for rot := 0; rot < 4; rot++ {
+		diff := difference(s, t) // 色変更回数
+		totalOps := diff + rot   // 総操作回数　＝　色変更＋回転回数
+		if totalOps < ops {
+			ops = totalOps
+		}
+		s = rotate90(s) // 次の回転状態
+	}
+	fmt.Println(ops)
 }
--- golang/template.go
+++ golang/template.go
@@ -1,2 +1,30 @@
 // Algorithm Lists
 package main
+
+// bufioテンプレ
+in  := bufio.NewReader(os.Stdin)
+out := bufio.NewWriter(os.Stdout)
+defer out.Flush()
+
+var n int
+fmt.Fscan(in, &n)
+
+
+
+// 二次元配列を作成したい時
+sc := bufio.NewScanner(os.Stdin)
+sc.Scan()
+var n int
+fmt.Sscanf(sc.Text(), "%d", &n)
+
+s := make([]string, n)
+for i := 0; i < n; i++ {
+	sc.Scan()
+	s[i] = sc.Text()
+}
+
+t := make([]string, n)
+for i := 0; i < n; i++ {
+	sc.Scan()
+	t[i] = sc.Text()
+}
