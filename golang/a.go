7f64ddd7b96c180d54051d45e6465b15ed0e4149
Fuji <fujiji.entire1414@gmail.com>
2025-05-06 21:05:07 +0900

    AOJ 1_9_D

--- golang/a.go
+++ golang/a.go
@@ -6,60 +6,33 @@ import (
 	"os"
 )
 
-func rotate90(grid []string) []string {
-	n := len(grid)
-	rotated := make([]string, n)
-	for i := 0; i < n; i++ {
-		row := make([]byte, n)
-		for j := 0; j < n; j++ {
-			// 90度回転の変換式：新しい[i][j] = 元の[n-j-1][i]
-			row[j] = grid[n-j-1][i]
-		}
-		rotated[i] = string(row)
-	}
-	return rotated
-}
-
-// gridの差分をとる
-func difference(a, b []string) int {
-	n := len(a)
-	count := 0
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			if a[i][j] != b[i][j] {
-				count++ // 色が違えば1回の変更が必要 => 必要変更回数をカウント
-			}
-		}
-	}
-	return count
-}
-
 func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	sc.Scan()
+	in := bufio.NewReader(os.Stdin)
+
+	var s string
 	var n int
-	fmt.Sscanf(sc.Text(), "%d", &n)
+	fmt.Fscan(in, &s)
+	fmt.Fscan(in, &n)
 
-	s := make([]string, n)
 	for i := 0; i < n; i++ {
-		sc.Scan()
-		s[i] = sc.Text()
-	}
+		var q string
+		var a, b int
+		fmt.Fscan(in, &q, &a, &b)
 
-	t := make([]string, n)
-	for i := 0; i < n; i++ {
-		sc.Scan()
-		t[i] = sc.Text()
-	}
-
-	ops := n * n * 10 //十分大きい初期値
-	for rot := 0; rot < 4; rot++ {
-		diff := difference(s, t) // 色変更回数
-		totalOps := diff + rot   // 総操作回数　＝　色変更＋回転回数
-		if totalOps < ops {
-			ops = totalOps
+		switch q {
+		case "print":
+			fmt.Println(s[a : b+1])
+			// 一文字ずつ扱うときは[]rune
+		case "reverse":
+			sub := []rune(s[a : b+1])
+			for i, j := 0, len(sub)-1; i < j; i, j = i+1, j-1 {
+				sub[i], sub[j] = sub[j], sub[i]
+			}
+			s = s[:a] + string(sub) + s[b+1:]
+		case "replace":
+			var p string
+			fmt.Fscan(in, &p)
+			s = s[:a] + p + s[b+1:]
 		}
-		s = rotate90(s) // 次の回転状態
 	}
-	fmt.Println(ops)
 }
