6bf360dd8c5106f27d76bc2064c60086757ee75e
Fuji <fujiji.entire1414@gmail.com>
2025-05-08 22:20:19 +0900

    三角関数

--- golang/a.go
+++ golang/a.go
@@ -7,17 +7,24 @@ import (
 	"os"
 )
 
-func distance(x1, y1, x2, y2 float64) float64 {
-	dx := x1 - x2
-	dy := y1 - y2
-	return math.Sqrt(dx*dx + dy*dy)
+func zhou_length(a, b, C float64) float64 {
+	var c float64
+	c = a*a + b*b - 2*a*b*math.Cos(C)
+	return math.Sqrt(c)
 }
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
-	var x1, y1, x2, y2 float64
-	fmt.Fscan(in, &x1, &y1, &x2, &y2)
+	var a, b, C float64
+	fmt.Fscan(in, &a, &b, &C)
 
-	d := distance(x1, y1, x2, y2)
-	fmt.Printf("%.8f\n", d)
+	C = C * math.Pi / 180
+
+	s := a * b * math.Sin(C) / 2
+	h := 2 * s / a
+	l := zhou_length(a, b, C) + a + b
+
+	fmt.Printf("%.8f\n", s)
+	fmt.Printf("%.8f\n", l)
+	fmt.Printf("%.8f\n", h)
 }
