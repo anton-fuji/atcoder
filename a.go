93fa438a7c93a32a3b5d8f2ad50ec69d3f82af7c
Fuji <fujiji.entire1414@gmail.com>
2025-05-16 19:09:23 +0900

    Judge Prime number

--- a.go
+++ a.go
@@ -1,56 +1,34 @@
-pckage main
+package main
 
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 )
-
-func abs() {
-  
-}
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	var n int
 	fmt.Fscan(in, &n)
 
-  h := make([]int, n)	
-	for i := 0; i < n; i++ {
-		fmt.Fscan(in, &h[i])
+	if n <= 1 {
+		return
 	}
 
-  
-	dp := make([]int, n)
-	for i := range dp {
-		dp[i] = inf
-	}
-	dp[0] = 0
-
-	for i := 0; i < n; i++ {
-		if i+1 < n {
-			d := dp[i] + abs(h[i+1]-h[i])
-			if d < dp[i+1] {
-				dp[i+1] = d
-			}
-		}
-
-		if i+2 < n {
-			d := dp[i] + abs(h[i+2]-h[i])
-			if d < dp[i+2] {
-				dp[i+2] = d
-			}
+	isPrime := true
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			isPrime = false
+			break
 		}
 	}
 
-	fmt.Println(dp[n-1])
-
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
+	if isPrime {
+		fmt.Printf("%d is a prime number", n)
+	} else {
+		fmt.Printf("%d is not a prime number", n)
 	}
-	return x
 
 }
+
