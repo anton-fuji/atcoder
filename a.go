f5a85db953942d4a01899c6688162ba38b7a5b3d
Fuji <fujiji.entire1414@gmail.com>
2025-07-09 23:09:00 +0900

    Paiza_C問題

--- a.go
+++ a.go
@@ -1,46 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	out := bufio.NewWriter(os.Stdout)
-	defer out.Flush()
+	var s string
+	fmt.Scan(&s)
 
-	var h, w int
-	fmt.Fscan(in, &h, &w)
-
-	a := make([][]int64, h)
-	for i := 0; i < h; i++ {
-		a[i] = make([]int64, w)
-		for j := 0; j < w; j++ {
-			fmt.Fscan(in, &a[i][j])
-		}
+	length := len(s) + 2
+	for i := 0; i < length; i++ {
+		fmt.Print("+")
 	}
-
-	rowSum := make([]int64, h)
-	colSum := make([]int64, w)
-	for i := 0; i < h; i++ {
-		for j := 0; j < w; j++ {
-			v := a[i][j]
-			rowSum[i] += v
-			colSum[j] += v
-		}
+	fmt.Println()
+	fmt.Printf("+%s+\n", s)
+	for i := 0; i < length; i++ {
+		fmt.Print("+")
 	}
-
-	for i := 0; i < h; i++ {
-		for j := 0; j < w; j++ {
-			ans := rowSum[i] + colSum[j] - a[i][j]
-			fmt.Fprint(out, ans)
-			if j < w-1 {
-				out.WriteByte(' ')
-			}
-		}
-		out.WriteByte('\n')
-	}
-
+	fmt.Println()
 }
