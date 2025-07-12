f73ddef2a01970b54a2c74c3450a4407e5586ed5
Fuji <fujiji.entire1414@gmail.com>
2025-07-12 13:56:55 +0900

    Go-Algo復習

--- tmpl/template.go
+++ tmpl/template.go
@@ -28,3 +28,29 @@ for i := 0; i < n; i++ {
 	sc.Scan()
 	t[i] = sc.Text()
 }
+
+// 行列の作成
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var h, w int
+	fmt.Fscan(in, &h, &w)
+
+	a := make([][]int64, h)
+	for i := 0; i < h; i++ {
+		a[i] = make([]int64, w)
+		for j := 0; j < w; j++ {
+			fmt.Fscan(in, &a[i][j])
+		}
+	}
+
+	rowSum := make([]int64, h)
+	colSum := make([]int64, w)
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			v := a[i][j]
+			rowSum[i] += v
+			colSum[j] += v
+		}
+	}
