b5703d28917d7cbb7d06212815b0a0cf3edf8555
Fuji <fujiji.entire1414@gmail.com>
2025-07-17 13:00:14 +0900

    Go-Alog復習

--- a.go
+++ a.go
@@ -2,46 +2,23 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
+func countStrings(s []string) []string {
+	count := map[string]bool{}
+	result := []string{}
+
+	for _, v := range s {
+		if !count[v] {
+			count[v] = true
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
 func main() {
-	var n int
-	fmt.Scan(&n)
-	matrix := make([][]int, n)
-	for i := 0; i < n; i++ {
-		matrix[i] = make([]int, n)
-		for j := 0; j < n; j++ {
-			fmt.Scan(&matrix[i][j])
-		}
-	}
+	s := []string{"apple", "grape", "banana", "apple"}
 
-	var peak []int
-	dx := []int{0, 1, 0, -1}
-	dy := []int{-1, 0, 1, 0}
-
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			curr := matrix[i][j]
-			isPeak := true
-			for d := 0; d < 4; d++ {
-				ni := i + dy[d]
-				nj := j + dx[d]
-				if ni >= 0 && ni < n && nj >= 0 && nj < n {
-					if matrix[ni][nj] >= curr {
-						isPeak = false
-						break
-					}
-				}
-			}
-			if isPeak {
-				peak = append(peak, curr)
-			}
-		}
-	}
-	sort.Sort(sort.Reverse(sort.IntSlice(peak)))
-
-	for _, h := range peak {
-		fmt.Println(h)
-	}
+	fmt.Println(countStrings(s))
 }
