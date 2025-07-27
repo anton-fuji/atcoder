6f8bd912c771b71061aee5ddffc0d0ceb9cddfcb
Fuji <fujiji.entire1414@gmail.com>
2025-07-27 17:25:04 +0900

    ABC416_B復習

--- b.go
+++ b.go
@@ -1,14 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
+	input := bufio.NewReader(os.Stdin)
+	var s string
+	fmt.Fscan(input, &s)
 
-	for i := 1; i <= 9; i++ {
-		for j := 1; j <= 9; j++ {
-			fmt.Printf("%dx%d=%2d ", j, i, i*j)
+	n := len(s)
+	t := make([]rune, n)
+
+	// t に対して#, .で初期化
+	for i, v := range s {
+		if v == '#' {
+			t[i] = '#'
+		} else {
+			t[i] = '.'
 		}
-		fmt.Println()
 	}
 
+	// 最後にoを配置したインデックスを記録
+	lastIdx := -1
+	// 最後に#が見つかったインデックスを記録
+	lastHash := -1
+
+	for i := 0; i < n; i++ {
+		if s[i] == '#' {
+			t[i] = '#'
+			lastHash = i
+		} else {
+			if lastIdx == -1 || lastHash > lastIdx {
+				t[i] = 'o'
+				lastIdx = i
+			} else {
+				t[i] = '.'
+			}
+		}
+	}
+
+	fmt.Println(string(t))
 }
