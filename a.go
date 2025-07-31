b926b99d8c6ef299ff1b9bfb28d0273ef0872607
Fuji <fujiji.entire1414@gmail.com>
2025-07-31 15:10:43 +0900

    ABC396_B

--- a.go
+++ a.go
@@ -7,20 +7,34 @@ import (
 )
 
 func main() {
-	input := bufio.NewReader(os.Stdin)
-	var n int
-	var s, t string
-	var cnt int
+	var q int
+	fmt.Scan(&q)
 
-	fmt.Fscan(input, &n)
-	fmt.Fscan(input, &s, &t)
+	stack := make([]int, 100)
+	for i := range stack {
+		stack[i] = 0
+	}
 
-	runes_S := []rune(s)
-	runes_T := []rune(t)
-	for i := 0; i < n; i++ {
-		if runes_S[i] != runes_T[i] {
-			cnt++
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+
+	nexInt := func() int {
+		sc.Scan()
+		var x int
+		fmt.Sscan(sc.Text(), &x)
+		return x
+	}
+
+	for i := 0; i < q; i++ {
+		cmd := nexInt()
+		if cmd == 1 {
+			x := nexInt()
+			stack = append(stack, x)
+		} else if cmd == 2 {
+			top := stack[len(stack)-1]
+			fmt.Println(top)
+			stack = stack[:len(stack)-1]
 		}
 	}
-	fmt.Println(cnt)
+
 }
